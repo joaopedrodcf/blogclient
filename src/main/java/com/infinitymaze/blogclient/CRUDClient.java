9b6b0410a91413015374b150a6363cb3de80e27d
Joao Ferreira <joaoferr93@gmail.com>
2018-03-04 14:15:14 +0000

    Added some exception handling to the client for the case of 404

--- src/main/java/com/infinitymaze/blogclient/CRUDClient.java
+++ src/main/java/com/infinitymaze/blogclient/CRUDClient.java
@@ -2,6 +2,7 @@ package com.infinitymaze.blogclient;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -13,55 +14,62 @@ public class CRUDClient<T> {
 	private Class<T> typeParameterClass;
 
 	private Class<T[]> typeParameterArrayClass;
-	
+
 	private static RestTemplate restTemplate = new RestTemplate();
-	
+
 	private static final Logger logger = LogManager.getLogger(CRUDClient.class);
-	
-	public CRUDClient(String restServiceUrl, Class<T> typeParameterClass, Class<T[]> typeParameterArrayClass){
-		this.restServiceUrl=restServiceUrl;
+
+	public CRUDClient(String restServiceUrl, Class<T> typeParameterClass, Class<T[]> typeParameterArrayClass) {
+		this.restServiceUrl = restServiceUrl;
 		this.typeParameterClass = typeParameterClass;
 		this.typeParameterArrayClass = typeParameterArrayClass;
 	}
 
 	public void getAll() {
 		ResponseEntity<T[]> response = restTemplate.getForEntity(restServiceUrl, typeParameterArrayClass);
-
-		logger.info(response.getStatusCodeValue());
+		logger.info(typeParameterClass + " " + response.getStatusCodeValue());
 		for (T t : response.getBody()) {
-			logger.info(t);
+			logger.info(typeParameterClass.getName() + " " + t);
 		}
 	}
 
 	public void deleteAll() {
-		restTemplate.delete(restServiceUrl);
+		try {
+			ResponseEntity<Void> response = restTemplate.exchange(restServiceUrl, HttpMethod.DELETE, null,
+					Void.class);
+			logger.info(typeParameterClass.getName() + " " + response.getStatusCodeValue());
+		} catch (final HttpClientErrorException e) {
+			logger.info(typeParameterClass.getName() + " " + e.getStatusCode());
+		}
 	}
 
 	public void delete(long id) {
 		try {
-			restTemplate.delete(restServiceUrl + id);
+			ResponseEntity<Void> response = restTemplate.exchange(restServiceUrl + id, HttpMethod.DELETE, null,
+					Void.class);
+			logger.info(typeParameterClass.getName() + " " + response.getStatusCodeValue());
 		} catch (final HttpClientErrorException e) {
-			logger.info(e.getStatusCode());
+			logger.info(typeParameterClass.getName() + " " + e.getStatusCode());
 		}
 	}
 
 	public void get(long id) {
 		ResponseEntity<T> response = restTemplate.getForEntity(restServiceUrl + id, typeParameterClass);
-		logger.info(response.getBody());
-		logger.info(response.getStatusCodeValue());
+		logger.info(typeParameterClass.getName() + " " + response.getBody());
+		logger.info(typeParameterClass.getName() + " " + response.getStatusCodeValue());
 
 	}
 
 	public void create(T t) {
 		ResponseEntity<Void> response = restTemplate.postForEntity(restServiceUrl, t, Void.class);
-		logger.info(response.getStatusCodeValue());
+		logger.info(typeParameterClass.getName() + " " + response.getStatusCodeValue());
 	}
 
 	public void update(long id, T t) {
 		try {
 			restTemplate.put(restServiceUrl + id, t);
 		} catch (final HttpClientErrorException e) {
-			logger.info(e.getStatusCode());
+			logger.info(typeParameterClass.getName() + " " + e.getStatusCode());
 		}
 	}
 }
